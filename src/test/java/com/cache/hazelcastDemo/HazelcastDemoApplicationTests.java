6eb983d423933670532d2519132fe379a88c43a8
rizkiauliasa <rizkiauliasa@student.uns.ac.id>
2025-08-22 00:21:29 +0700

    Initialize project.

---
+++ src/test/java/com/cache/hazelcastDemo/HazelcastDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cache.hazelcastDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HazelcastDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
